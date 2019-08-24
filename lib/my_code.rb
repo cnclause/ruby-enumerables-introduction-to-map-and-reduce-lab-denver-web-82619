cff4bbf8b0df4ba5dbbd02b8f10d51dc1fe656a6
Catherine Clause <cnclause123@gmail.com>
2019-08-24 17:36:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,7 @@ end
 def reduce_to_all_true(source_array)  
   counter = 0 
   while counter < source_array.length do 
-    return false if  
+    return false if (source_array[counter] == false) 
     counter +=1 
   end 
   return true
